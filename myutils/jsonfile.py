d09a293f28714996333c9930ae89a7065b4e1486
Joel <joel.edwardson1@gmail.com>
2020-10-26 11:24:31 +0000

    json_file logs error rather than crashing on serialisation fail

--- myutils/jsonfile.py
+++ myutils/jsonfile.py
@@ -1,17 +1,32 @@
 import json
+import logging
+
+active_logger = logging.getLogger(__name__)
 
 
 def add_to_file(file_path, data):
     """add a serializable data object as a new line to a file"""
     with open(file_path, mode='a') as f:
-        f.writelines([json.dumps(data) + '\n'])
+        try:
+            json_data = json.dumps(data)
+        except  TypeError as e:
+            active_logger.critical(f'failed to serialise data: "{data}"\n{e}')
+            return
+
+        f.writelines([json_data + '\n'])
 
 
 def read_file(file_path):
     """get a list of de-serialized objects from each line in a file"""
     with open(file_path) as f:
         lines = f.readlines()
-    return [json.loads(line) for line in lines]
+
+    try:
+        json_data = [json.loads(line) for line in lines]
+        return json_data
+    except TypeError as e:
+        active_logger.critical(f'failed to load data: "{data}"\ne')
+        return []
 
 
 def is_jsonable(x):
