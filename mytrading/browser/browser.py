86e1de031dbaeb5f47b3e7ee19931ec21f1fda91
Joel <joel.edwardson1@gmail.com>
2020-10-27 12:03:17 +0000

    browser called via function, cannot be called from within mytading anymore as using relative references

--- mytrading/browser/browser.py
+++ mytrading/browser/browser.py
@@ -7,36 +7,18 @@ from .callbacks import market_callback, file_table_callback, figure_callback, or
 from .layout import get_layout
 from datetime import timedelta
 
-DEFAULT_CHART_OFFSET = timedelta(minutes=3)
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter  # show defaults in help
-)
-parser.add_argument(
-    'input_dir',
-    type=str,
-    help='input directory of markets'
-)
-parser.add_argument(
-    '--debug',
-    action='store_true',
-    help='run in debug mode'
-)
+def run_browser(debug: bool, default_chart_offset: timedelta, input_dir: str):
+    gdd = DashData(input_dir)
 
-args = parser.parse_args()
-input_dir = args.input_dir
-gdd = DashData(input_dir)
+    app = dash.Dash(__name__)
+    logging.basicConfig(level=logging.INFO)
+    app.layout = get_layout(input_dir, gdd, default_chart_offset)
 
-app = dash.Dash(__name__)
-logging.basicConfig(level=logging.INFO)
-app.layout = get_layout(input_dir, gdd, DEFAULT_CHART_OFFSET)
+    file_table_callback(app, gdd, input_dir)
+    market_callback(app, gdd, input_dir)
+    figure_callback(app, gdd, input_dir)
+    orders_callback(app, gdd, input_dir)
 
-file_table_callback(app, gdd, input_dir)
-market_callback(app, gdd, input_dir)
-figure_callback(app, gdd, input_dir)
-orders_callback(app, gdd, input_dir)
-
-
-if __name__ == '__main__':
     # turn of dev tools prop check to disable time input error
-    app.run_server(debug=args.debug, dev_tools_props_check=False)
+    app.run_server(debug=debug, dev_tools_props_check=False)
