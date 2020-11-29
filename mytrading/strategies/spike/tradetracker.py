59b4175d7d50882629ee12b71be643cced4046de
Joel <joel.edwardson1@gmail.com>
2020-11-29 13:47:55 +0000

    added previous value boundary max/min to tradetracker

--- mytrading/strategies/spike/tradetracker.py
+++ mytrading/strategies/spike/tradetracker.py
@@ -1,16 +1,21 @@
 from ...tradetracker.tradetracker import TradeTracker
 from flumine.order.order import OrderStatus, BetfairOrder
+from dataclasses import dataclass, field
 
 
 class SpikeTradeTracker(TradeTracker):
 
     # back and lay orders
-    back_order: BetfairOrder
-    lay_order: BetfairOrder
+    back_order: BetfairOrder = field(default=None)
+    lay_order: BetfairOrder = field(default=None)
 
     # side of book which spike order has money matched
-    side_matched: str
+    side_matched: str = field(default='')
 
     # ltp at point of spike
-    spike_ltp: float
+    spike_ltp: float = field(default=0)
+
+    # track previous state minimum/max odds of which offset applied to place spike orders
+    previous_max_index: int = field(default=0)
+    previous_min_index: int = field(default=0)
 
