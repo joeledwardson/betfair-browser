0953e79dbe9c5bd95e80ae70f45c9371f21abf4e
Joel <joel.edwardson1@gmail.com>
2020-12-18 18:24:51 +0000

    added helper function `plotly_table` which creates a plotly table from dataframe and adds title to figure

--- myutils/myplotly/table.py
+++ myutils/myplotly/table.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.graph_objs as go
 
 
 def plotly_table_kwargs(df: pd.DataFrame) -> dict:
@@ -7,3 +8,12 @@ def plotly_table_kwargs(df: pd.DataFrame) -> dict:
         header=dict(values=list(df.columns)),
         cells=dict(values=[df[c].to_list() for c in df.columns])
     )
+
+
+def plotly_table(df: pd.DataFrame, title: str) -> go.Figure:
+    """create plotly.Figure object for table with dataframe data and title"""
+    return go.Figure(
+        data=go.Table(**plotly_table_kwargs(df)),
+        layout=dict(title=title),
+    )
+
