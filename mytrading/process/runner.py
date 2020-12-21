7077c5daaa069ee4a74f27e4ee9a17ab6be2eabf
Joel <joel.edwardson1@gmail.com>
2020-12-21 14:39:12 +0000

    Updated type hints

--- mytrading/process/runner.py
+++ mytrading/process/runner.py
@@ -1,10 +1,10 @@
-from typing import Union, List
+from typing import Union, List, Optional
 
 from betfairlightweight.resources import MarketDefinitionRunner
 from betfairlightweight.resources.bettingresources import RunnerBook
 
 
-def get_book(runners: Union[List[RunnerBook], List[MarketDefinitionRunner]], selection_id) -> RunnerBook:
+def get_book(runners: Union[List[RunnerBook], List[MarketDefinitionRunner]], selection_id) -> Optional[RunnerBook]:
     """
     Get a runner object by checking for match of "selection_id" attribute from a list of objects
     """
