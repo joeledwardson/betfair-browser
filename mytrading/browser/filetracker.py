736d71c6fb5fce1e186175abf66a83c27627936c
Joel <joel.edwardson1@gmail.com>
2021-01-02 10:34:42 +0000

    consolidated update functionality into single `update()` function can be used internally and externally alike

--- mytrading/browser/filetracker.py
+++ mytrading/browser/filetracker.py
@@ -20,10 +20,17 @@ class FileTracker:
         # combination of dirs and files
         self.elements: List[str] = list()
 
-        self.root, self.dirs, self.files, self.elements = self._get_update(start_dir)
-        self.display_list = self.get_display_list()
+        self.display_list = []
+        self.update(start_dir)
 
     def _get_update(self, top) -> Tuple[str, List, List, List]:
+        """
+        returns list:
+        1. `top` (arg passed), the root path to get files and dirs from
+        2. list of dirs
+        3. list of files
+        4. list of elements (combination of dirs then files)
+        """
 
         # check top path exists
         if not path.exists(top):
@@ -40,6 +47,13 @@ class FileTracker:
         elements = list(chain(dirs, files))
         return root, dirs, files, elements
 
+    def update(self, top):
+        """
+        update file tracker from `top` new path, setting file tracker root, dirs, files and elements
+        """
+        self.root, self.dirs, self.files, self.elements = self._get_update(top)
+        self.display_list = self.get_display_list()
+
     def get_display_list(self):
         """list of dirs first, then files with symbols to display in table"""
         return ['📁 ' + f for f in self.dirs] + ['🗎 ' + f for f in self.files]
@@ -48,14 +62,11 @@ class FileTracker:
         """navigate to directory based on its index in display list"""
         if 0 <= index < len(self.dirs):
             selected_dir = self.dirs[index]
-            self.root, self.dirs, self.files, self.elements = self._get_update(path.join(self.root, selected_dir))
-
-            self.display_list = self.get_display_list()
+            self.update(path.join(self.root, selected_dir))
 
     def navigate_up(self):
         """return to parent directory"""
-        self.root, self.dirs, self.files, self.elements = self._get_update(path.split(self.root)[0])
-        self.display_list = self.get_display_list()
+        self.update(path.split(self.root)[0])
 
     def get_file_name(self, file_display_index):
         """get file name from its index in display list"""
