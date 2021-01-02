918941f99063aaf7c12cd51b850e860bfad1302a
Joel <joel.edwardson1@gmail.com>
2021-01-02 16:42:44 +0000

    updated trend data type to include max difference in ticks and movement

--- mytrading/strategies/trend/datatypes.py
+++ mytrading/strategies/trend/datatypes.py
@@ -12,19 +12,34 @@ class TrendData:
 
     best_back: float
     best_lay: float
-    ladder_spread_ticks: int
     ltp: float
+
+    ladder_spread_ticks: int
 
     smoothed_back: float
     smoothed_lay: float
     smoothed_ltp: float
+
+    back_tick_movement: int
+    lay_tick_movement: int
+    ltp_tick_movement: int
+
+    back_max_diff_ticks: int
+    lay_max_diff_ticks: int
+    ltp_max_diff_ticks: int
+
+    @property
+    def ok(self):
+        return all([v is not None for v in self.__dict__.values()])
 
 
 @dataclass
 class TrendCriteria:
     ladder_gradient_min: float
     ladder_strength_min: float
+    ladder_jump_max: int
     ltp_gradient_min: float
     ltp_strength_min: float
+    ltp_jump_max: int
     ladder_spread_max: float
 
