74f1333422714e49497d8a74a4de007d9d40cb5b
Joel <joel.edwardson1@gmail.com>
2021-01-02 02:15:29 +0000

    added getter

--- myutils/myregistrar.py
+++ myutils/myregistrar.py
@@ -1,3 +1,7 @@
+class RegistrarException(Exception):
+    pass
+
+
 class MyRegistrar:
     def __init__(self):
         self.register = dict()
@@ -7,7 +11,15 @@ class MyRegistrar:
         register an element, add to dictionary of elements
         """
         if obj.__name__ in self.register:
-            raise Exception(f'registering feature "{obj.__name__}", but already exists!')
+            raise Exception(f'registering object "{obj.__name__}", but already exists!')
         else:
             self.register[obj.__name__] = obj
             return obj
+
+    def __getitem__(self, item):
+        if item in self.register:
+            return self.register[item]
+        else:
+            raise RegistrarException(f'object "{item}" not found in registrar')
+
+
