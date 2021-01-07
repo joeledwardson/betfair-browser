7ca8a212b32533d3ad59131613f067f098da6048
Joel <joel.edwardson1@gmail.com>
2021-01-07 21:42:36 +0000

    deleted importer module

--- myutils/importer.py
+++
@@ -1,9 +0,0 @@
-from . import generic, betting, mytiming
-import importlib
-from datetime import datetime
-
-
-def reload_utils():
-    for lib in [generic, betting, mytiming]:
-        importlib.reload(lib)
-    print(f'{datetime.now().strftime("%y-%m-%d %H-%M-%S")} - utils lib reloaded')
