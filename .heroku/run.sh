5e9e5a66bb88ef07d58a57667507bef1a7631044
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-11-23 20:18:03 +0000

    updated heroku run script to install poetry

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,2 +1,2 @@
-python -m pip install poetry
-poetry export -f requirements.txt -o requirements.txt --without-hashes
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+$HOME/.poetry/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes
