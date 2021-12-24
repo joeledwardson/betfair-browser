c1bf2254d8538c1ada9e66c8c0fb7724917c551a
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-12-24 23:07:50 +0000

    updated shell script

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,2 +1,10 @@
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+echo "****************** exporting from poetry to requirements.txt"
+echo "installing poetry..."
+curl -sSL https://install.python-poetry.org | python3 -
+#curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+echo "finished poetry instalation..."
+echo "poetry version:"
+$HOME/.poetry/bin/poetry --version
+echo "exporting..."
 $HOME/.poetry/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes
+echo "****************** done!"
