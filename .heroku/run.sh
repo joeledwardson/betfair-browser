1e2124d6bb91227d2f2be2c31854662a19ad7601
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-12-24 23:17:06 +0000

    corrected path bug in heroku script

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,16 +1,22 @@
-echo "****************** exporting from poetry to requirements.txt"
+echo "******************"
+echo "exporting from poetry to requirements.txt"
 echo "installing poetry..."
 curl -sSL https://install.python-poetry.org | python3 -
 #curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-printf "finished poetry installation..."
+echo "finished poetry installation..."
 
-printf "\nadding poetry to path"
-export PATH="/$HOME/.local/bin:$PATH"
+echo
+echo "adding poetry to path"
+export PATH="$HOME/.local/bin:$PATH"
 
+echo
 echo "poetry version:"
 poetry --version
 
-printf "\nexporting..."
+echo
+echo "exporting..."
 poetry export -f requirements.txt -o requirements.txt --without-hashes
 
-printf "\n****************** done!"
+echo
+printf "done!"
+echo "******************"
