4bee812f76299962f75623879fe31b9ce94ffa2f
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-12-24 23:39:56 +0000

    re-added lock so poetry can export without need for a venv

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,23 +1,18 @@
 echo "******************"
 echo "exporting from poetry to requirements.txt"
-sudo apt install python3.8-venv
+
 echo "installing poetry..."
 curl -sSL https://install.python-poetry.org | python3 - --preview
-#curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 echo "finished poetry installation..."
 
-#echo
-#echo "adding poetry to path"
-#export PATH="$HOME/.local/bin:$PATH"
-
-echo -e "\r\n"
+echo -e " \r\n"
 echo "poetry version:"
 $HOME/.local/bin/poetry --version
 
-echo -e "\r\n"
+echo -e " \r\n"
 echo "exporting..."
 $HOME/.local/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes
 
-echo -e "\r\n"
+echo -e " \r\n"
 printf "done!"
 echo "******************"
