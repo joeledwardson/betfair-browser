ab708ee08cae23019381077bca54c9aa59238b8d
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-12-24 23:52:13 +0000

    modified run to only check for requirements.txt

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,18 +1,7 @@
-echo "******************"
-echo "exporting from poetry to requirements.txt"
-
-echo "installing poetry..."
-curl -sSL https://install.python-poetry.org | python3 - --preview
-echo "finished poetry installation..."
-
-echo -e " \r\n"
-echo "poetry version:"
-$HOME/.local/bin/poetry --version
-
-echo -e " \r\n"
-echo "exporting..."
-$HOME/.local/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes
-
-echo -e " \r\n"
-printf "done!"
-echo "******************"
+echo "checking requirements.txt exists"
+if [ -f requirements.txt ]; then
+   echo "requirements.txt does exist, continuing..."
+else
+   echo "could not find requirements.txt!"
+   exit 1
+fi
