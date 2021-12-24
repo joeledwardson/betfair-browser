29b985fb283f2aa22a74e1f465f63a5ac316b482
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-12-24 23:14:41 +0000

    updated shell script to use new style

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -2,9 +2,15 @@ echo "****************** exporting from poetry to requirements.txt"
 echo "installing poetry..."
 curl -sSL https://install.python-poetry.org | python3 -
 #curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-echo "finished poetry instalation..."
+printf "finished poetry installation..."
+
+printf "\nadding poetry to path"
+export PATH="/$HOME/.local/bin:$PATH"
+
 echo "poetry version:"
-$HOME/.poetry/bin/poetry --version
-echo "exporting..."
-$HOME/.poetry/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes
-echo "****************** done!"
+poetry --version
+
+printf "\nexporting..."
+poetry export -f requirements.txt -o requirements.txt --without-hashes
+
+printf "\n****************** done!"
