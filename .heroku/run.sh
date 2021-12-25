e0d31f70b0a82db9e5a9f3ddce30c0f55168aa57
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-12-25 00:14:14 +0000

    re-added runtime and poetry script

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,3 +1,19 @@
+echo "******************"
+echo "exporting from poetry to requirements.txt"
+
+echo "installing poetry..."
+curl -sSL https://install.python-poetry.org | python3 - --preview
+echo "finished poetry installation..."
+
+echo -e " \r\n"
+echo "poetry version:"
+$HOME/.local/bin/poetry --version
+
+echo -e " \r\n"
+echo "exporting..."
+$HOME/.local/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes
+
+echo -e " \r\n"
 echo "checking requirements.txt exists"
 if [ -f requirements.txt ]; then
    echo "requirements.txt does exist, continuing..."
@@ -5,3 +21,7 @@ else
    echo "could not find requirements.txt!"
    exit 1
 fi
+
+echo -e " \r\n"
+echo "done!"
+echo "******************"
