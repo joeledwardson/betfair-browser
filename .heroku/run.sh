8e7a7155da3ece9718edd75aa721ab9192c8e40f
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-12-25 14:39:12 +0000

    used old poetry installation script, removed aptfile for apt buildpack

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -2,16 +2,16 @@ echo "******************"
 echo "exporting from poetry to requirements.txt"
 
 echo "installing poetry..."
-curl -sSL https://install.python-poetry.org | python3 - --preview
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 echo "finished poetry installation..."
 
 echo -e " \r\n"
 echo "poetry version:"
-$HOME/.local/bin/poetry --version
+$HOME/.poetry/bin/poetry --version
 
 echo -e " \r\n"
 echo "exporting..."
-$HOME/.local/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes
+$HOME/.poetry/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes
 
 echo -e " \r\n"
 echo "checking requirements.txt exists"
